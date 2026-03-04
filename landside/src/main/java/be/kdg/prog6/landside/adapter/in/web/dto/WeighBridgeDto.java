87a890ca799171b1c3d9b5073a05044602194d04
ablak_huseyin <huseyin.ablak@student.kdg.be>
2026-03-04 15:07:06 +0100

    WeighBridgeDto: UUID -> String for 'id' field.

--- landside/src/main/java/be/kdg/prog6/landside/adapter/in/web/dto/WeighBridgeDto.java
+++ landside/src/main/java/be/kdg/prog6/landside/adapter/in/web/dto/WeighBridgeDto.java
@@ -5,14 +5,14 @@ import be.kdg.prog6.landside.domain.WeighBridge;
 import java.util.UUID;
 
 public record WeighBridgeDto(
-    UUID id,
+    String id,
     String number,
     UUID occupiedByVisitId
 ) {
     public static WeighBridgeDto fromDomain(final WeighBridge weighBridge) {
         final boolean isOccupied = weighBridge.isOccupied();
         return new WeighBridgeDto(
-            weighBridge.getId().id(),
+            weighBridge.getId().id().toString(),
             weighBridge.getNumber().value(),
             isOccupied ? weighBridge.getOccupiedByVisitId().id() : null
         );
